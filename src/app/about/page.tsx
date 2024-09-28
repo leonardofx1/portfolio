2919ca8a9e01481287fafe32ac97bcc59aab577b
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-09-28 06:07:45 -0300

    config distDir next

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -41,7 +41,14 @@ export default function About() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
             officiis mollitia. Repellat minus maiores tenetur sit nulla?
             Repellendus, fugiat, veniam consequatur quae quisquam accusantium
-            voluptates placeat quasi itaque blanditiis expedita.
+            voluptates placeat quasi itaque blanditiis expedita. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Maxime et accusantium
+            quod iusto. Pariatur expedita numquam odit ratione, labore eligendi
+            quos laborum dolores ea atque cupiditate hic fugiat impedit
+            asperiores. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Excepturi hic, quod animi mollitia autem labore velit nemo illo
+            doloribus ipsum vel magni nisi fuga error quos repellendus corrupti
+            optio minima?
           </p>
         </div>
         <div>
