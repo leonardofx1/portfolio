5852939a540ba41654fd58955f22c411a006e54d
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-09-28 05:46:27 -0300

    add about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,58 @@
+import styles from "./styles.module.scss";
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <section className={styles.wrapperAbout}>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            officiis mollitia. Repellat minus maiores tenetur sit nulla?
+            Repellendus, fugiat, veniam consequatur quae quisquam accusantium
+            voluptates placeat quasi itaque blanditiis expedita.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-import Link from 'next/link'
-import styles from './styles.module.scss'
+import Link from "next/link";
+import styles from "./styles.module.scss";
 export const Header = () => {
-
-    return (
+  return (
     <header className={styles.headerBody}>
-        <h1>Dev Fullstack</h1>
-        <nav>
-            <ul className={styles.headerList}>
-            <li className={styles.active}><Link href={''}>Contatos</Link></li>
-            <li> Inicio</li>
-            <li>Sobre</li>
-            <li>Projetos</li>
-            </ul>
-        </nav>
-    </header> 
-    )
-}
+      <h1>Dev Fullstack</h1>
+      <nav>
+        <ul className={styles.headerList}>
+          <li className={styles.active}>
+            <Link href={""}>Contatos</Link>
+          </li>
+          <li> Inicio</li>
+          <li> <Link href="./about">Sobre</Link></li>
+          <li>Projetos</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
