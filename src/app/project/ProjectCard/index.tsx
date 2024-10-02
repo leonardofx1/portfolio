72059c30044522127dd36cb1deb88430f03db8c2
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-10-02 12:34:58 -0300

    animate card project

---
+++ src/app/project/ProjectCard/index.tsx
@@ -0,0 +1,32 @@
+import Image, { StaticImageData } from "next/image";
+import styles from "../styles.module.scss";
+import * as motion from "framer-motion/client";
+import Link from "next/link";
+interface CardProps {
+  link: string;
+  imgSrc: StaticImageData;
+  title: string;
+}
+
+export const CardProject = ({ imgSrc, link, title }: CardProps) => {
+  return (
+    <motion.div
+      initial={{ x: -100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <Link href={link} className={styles.wrapperCard}>
+        <section className={styles.cardHeader}>
+          <Image src={imgSrc} alt="projeto fxmoney" />
+        </section>
+        <motion.h2
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          {title}
+        </motion.h2>
+      </Link>
+    </motion.div>
+  );
+};
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+
 import styles from "./styles.module.scss";
 
 import t from "@/assets/t.png";
-
+import { CardProject } from "./ProjectCard";
 
 export default function Project() {
   return (
@@ -10,16 +10,10 @@ export default function Project() {
       <h3>Projetos </h3>
 
       <section className={styles.wrapperProjects}>
-       
-      <div>
-          <article className={styles.wrapperCard}>
-            <section className={styles.cardHeader}>
-              <Image src={t} alt="projeto fxmoney" />
-            </section>
-            <h2>Fxmoney</h2>
-          </article>
-        </div>
+       <CardProject imgSrc={t} link="o"  title="oi"/>
       </section>
+
+      
     </main>
   );
 }
