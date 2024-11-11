0a11cdf2a475a42a4d1090bbf943387c2a93cb2c
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-11-11 01:04:43 -0300

    responsive home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,10 @@ export const Header = () => {
       <h1>Dev Fullstack</h1>
       <nav>
         <ul className={styles.headerList}>
-          <li className={styles.active}>
-            <Link href={""}>Contatos</Link>
-          </li>
-          <li> Inicio</li>
+          <li className={styles.active}> <Link href='/' >Inicio</Link></li>
+          
+           
+          
           <li> <Link href="./about">Sobre</Link></li>
           <li> <Link href='./project'>Projetos</Link></li>
         </ul>
